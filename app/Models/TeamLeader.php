057d5785fb9e8fa2425226e0ed05a88b4ffd8149
sabirkhaloufi <sabirkhaloufi@gmail.com>
2022-09-05 22:27:40 +0100

    add modification

--- app/Models/TeamLeader.php
+++ app/Models/TeamLeader.php
@@ -4,29 +4,44 @@ class TeamLeader
 {
 
     private $db;
+
     public function __construct( )    {
     $this->db = new Database;
-}
+    }
 
 
 
-public function getAllTeamLeader(){
-    $this->db->query("SELECT * FROM teamleaders;");
-    return $this->db->resultSet();
-}
+    public function getAllTeamLeader(){
+        $this->db->query("SELECT * FROM teamleaders;");
+        return $this->db->resultSet();
+    }
 
 
-public function getOpWithZone($idteam){
-    $this->db->query("SELECT operateurs.*,zones.designation
-    FROM operateurs
-    INNER JOIN zones
-    ON operateurs.fk_zone = zones.id AND zones.fk_teamLeader = $idteam;");
-    return $this->db->resultSet();  
-}
+    public function getOpWithZone($idteam){
+        $this->db->query("SELECT operateurs.*,zones.designation
+        FROM operateurs
+        INNER JOIN zones
+        ON operateurs.fk_zone = zones.id AND zones.fk_teamLeader = $idteam;");
+        return $this->db->resultSet();  
+    }
 
-public function getZone($idteam){
-    $this->db->query("SELECT designation from zones WHERE fk_teamLeader = $idteam");
-    return $this->db->resultSet();
-}
+    public function getZone($idteam){
+        $this->db->query("SELECT designation,time from zones WHERE fk_teamLeader = $idteam");
+        return $this->db->resultSet();
+    }
+
+    public function getNomTeam($id){
+        $this->db->query("SELECT nom,prenom from teamLeaders WHERE id = $id");
+        return $this->db->single();   
+    }
+
+    public function addTimeByZone($zone,$time){
+    // $zone = $_POST["zone"];
+    // $time = $_POST["time"];
+    $this->db->query("UPDATE `zones` SET time=:time WHERE designation = :zone");
+    $this->db->bind(':time' ,$time);
+    $this->db->bind(':zone' ,$zone);
+    $this->db->execute();
+    }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,28 +36,14 @@ public function badge($code,$date,$time){
     $this->db->bind(':time' ,$time);
     $this->db->bind(':code' ,$code);
     $this->db->execute();
+
+
+    $this->db->query("UPDATE `teamleaders` SET date=:date, time = :time WHERE code = :code");
+    $this->db->bind(':date' ,$date);
+    $this->db->bind(':time' ,$time);
+    $this->db->bind(':code' ,$code);
+    $this->db->execute();
 }
-
-
-public function update($data,$id,$imgName,$Fk_cat){
-    $this->db->query("UPDATE `techniciens` SET nom=:nom, prenom = :prenom,
-     email= :email, phone = :phone ,Fk_cat = :Fk_cat
-     , metier = :metier , password = :password , adresse = :adresse , ville = :ville , img=:img WHERE Id_tech =$id");
-    $this->db->bind(':nom' ,$data['nom']);
-    $this->db->bind(':prenom' ,$data['prenom']);
-    $this->db->bind(':email' ,$data['email']);
-    $this->db->bind(':phone' ,$data['phone']);
-    $this->db->bind(':metier' ,$data['job']);
-    $this->db->bind(':password' ,$data['password']);
-    $this->db->bind(':adresse' ,$data['adresse']);
-    $this->db->bind(':ville' ,$data['ville']);
-    $this->db->bind(':img' ,$imgName);
-    $this->db->bind(':Fk_cat' ,$Fk_cat);
-    $this->db->execute();  
-}
-
-
-
 
 
 }
